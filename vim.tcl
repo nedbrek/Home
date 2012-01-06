42e4fd8f33e904ee842dab85ae9570bb8b9d14de
Edward Brekelbaum <nedbrek@yahoo.com>
2012-01-06 18:28:02 -0500

    Vim collapse diff - added check for on diff

--- vim.tcl
+++ vim.tcl
@@ -3,13 +3,19 @@ proc collapseDiff {} {
    set l $::vim::range(begin)
    # Ned, check end==begin
 
+	# check that current line is a diff
+	set tmp [$b get $l]
+	if {[string index 0] eq ' '} {
+		return ;# not a diff line
+	}
+
    # retrieve current and next line
-   set curLine [string range [$b get $l] 1 end]
+   set curLine [string range $tmp 1 end]
    set nxtLine [string range [$b get [expr $l+1]] 1 end]
 
    # compare
    if {$curLine ne $nxtLine} {
-   return ;# different, leave intact
+      return ;# different, leave intact
    }
    # else, merge
 
