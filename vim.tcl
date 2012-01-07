07b2438988a63c77998be978b135235535f1e292
Edward Brekelbaum <nedbrek@yahoo.com>
2012-01-07 11:20:43 -0500

    vim.tcl: Expanded logic to check line above

--- vim.tcl
+++ vim.tcl
@@ -4,14 +4,39 @@ proc collapseDiff {} {
    # Ned, check end==begin
 
 	# check that current line is a diff
-	set tmp [$b get $l]
-	if {[string index $tmp 0] eq " "} {
+	set cur [$b get $l]
+	set firstChar [string index $cur 0]
+	if {$firstChar eq " "} {
 		return ;# not a diff line
 	}
 
-   # retrieve current and next line
-   set curLine [string range $tmp 1 end]
-   set nxtLine [string range [$b get [expr $l+1]] 1 end]
+	# find the other line, start with next
+	set oth [$b get [expr $l+1]]
+
+	if {$firstChar eq "+"} {
+		# should be -
+		if {[string index $oth 0] ne "-"} {
+			incr l -1
+			set oth [$b get $l]
+			if {[string index $oth 0] ne "-"} {
+				return
+			}
+		}
+	} else {
+		if {$firstChar ne "-"} { return } ;# something horribly wrong
+
+		if {[string index $oth 0] ne "+"} {
+			incr l -1
+			set oth [$b get $l]
+			if {[string index $oth 0] ne "+"} {
+				return
+			}
+		}
+	}
+
+   # retrieve current and other source line (strip lead char)
+   set curLine [string range $cur 1 end]
+   set nxtLine [string range $oth 1 end]
 
    # compare
    if {$curLine ne $nxtLine} {
